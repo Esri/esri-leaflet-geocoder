60040bf961d489d52685dfe01cd2f4e826e67e1e
john gravois <jgravois@esri.com>
2015-12-03 14:34:30 -0800

    revise getAttribution logic and make sure its called

--- spec/Controls/GeosearchSpec.js
+++ spec/Controls/GeosearchSpec.js
@@ -1,6 +1,24 @@
 
 describe('L.esri.Geosearch', function () {
 
+  function createMap(){
+    // create container
+    var container = document.createElement('div');
+
+    // give container a width/height
+    container.setAttribute('style', 'width:500px; height: 500px;');
+
+    // add contianer to body
+    document.body.appendChild(container);
+
+    return L.map(container, {
+      minZoom: 1,
+      maxZoom: 19
+    }).setView([45.51, -122.66], 5);
+  }
+
+  var map = createMap();
+
   it('shouldnt overwrite custom options set in the constructor', function() {
     var geosearch = L.esri.Geocoding.geosearch({
         providers: [
@@ -22,6 +40,17 @@ describe('L.esri.Geosearch', function () {
     expect(geosearch.options.allowMultipleResults).to.equal(false);
     expect(geosearch.options.placeholder).to.equal('something clever');
     expect(geosearch.options.title).to.equal('something not so clever');
+  });
+
+
+  it('should update map attribution when the World Geocoding Service is used', function() {
+    var geosearch = L.esri.Geocoding.geosearch({
+        providers: [
+          L.esri.Geocoding.arcgisOnlineProvider()
+        ]
+    }).addTo(map);
+
+    expect(map.attributionControl._container.innerHTML).to.contain('Geocoding by Esri');
   });
 
 });
