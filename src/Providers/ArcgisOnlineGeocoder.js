7690caf3651c95e8db94dec8afcac3d111556d37
jgravois <jgravois@esri.com>
2016-07-25 14:06:42 -0700

    maxResults has a default value

--- src/Providers/ArcgisOnlineGeocoder.js
+++ src/Providers/ArcgisOnlineGeocoder.js
@@ -23,9 +23,7 @@ export var ArcgisOnlineProvider = GeocodeService.extend({
     }
 
     // 15 is the maximum number of suggestions that can be returned
-    if (this.options.maxResults) {
-      request.maxSuggestions(this.options.maxResults);
-    }
+    request.maxSuggestions(this.options.maxResults);
 
     return request.run(function (error, results, response) {
       var suggestions = [];
@@ -51,9 +49,7 @@ export var ArcgisOnlineProvider = GeocodeService.extend({
       request.key(key);
     }
     // in the future Address/StreetName geocoding requests that include a magicKey will only return one match
-    if (this.options.maxResults) {
-      request.maxLocations(this.options.maxResults);
-    }
+    request.maxLocations(this.options.maxResults);
 
     if (bounds) {
       request.within(bounds);
