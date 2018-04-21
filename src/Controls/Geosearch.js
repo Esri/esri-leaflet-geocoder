8584a0be422c2ce474fb85c50002ecca9f158fd6
john gravois <jgravois@esri.com>
2018-04-21 15:53:22 -0700

    this is why pull requests are a good idea

--- src/Controls/Geosearch.js
+++ src/Controls/Geosearch.js
@@ -219,11 +219,11 @@ export var Geosearch = Control.extend({
     this._suggestions = DomUtil.create('div', 'geocoder-control-suggestions leaflet-bar', this._wrapper);
 
     var credits = this._geosearchCore._getAttribution();
-    
+
     if (map.attributionControl) {
       map.attributionControl.addAttribution(credits);
     }
-    
+
     DomEvent.addListener(this._input, 'focus', function (e) {
       this._input.placeholder = this.options.placeholder;
       DomUtil.addClass(this._wrapper, 'geocoder-control-expanded');
