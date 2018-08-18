d7a3f59eae5bfba53327f1d5ad580b6e10fe7c5f
john gravois <jgravois@esri.com>
2018-08-17 18:25:33 -0700

    ensure an external sourcemap is included in built output

--- profiles/base.js
+++ profiles/base.js
@@ -2,5 +2,6 @@ import config from '../node_modules/esri-leaflet/profiles/base.js';
 
 config.input = 'src/EsriLeafletGeocoding.js';
 config.output.name = 'L.esri.Geocoding';
+config.output.sourcemap = true;
 
 export default config;
--- profiles/debug.js
+++ profiles/debug.js
@@ -1,5 +1,6 @@
 import config from './base.js';
 
 config.output.file = 'dist/esri-leaflet-geocoder-debug.js';
+config.output.sourcemap = true;
 
 export default config;
