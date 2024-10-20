8ced6d39fe62c6c742c067b7234e73a8a2eaf972
nooras <anoorasfatima@gmail.com>
2024-10-20 15:39:32 +0530

    feat: TypeScript types file [#319]

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare module 'esri-leaflet-geocoder' {
+    export function geocode(options): any;
+    export var Geocode: any;
+    export function geocodeService(options): any;
+    export var GeocodeService: any;
+    export function geosearch(options): any;
+    export var Geosearch: any;
+    export function reverseGeocode(options): any;
+    export var ReverseGeocode: any;
+    export function suggest(options): any;
+    export var Suggest: any;
+}
